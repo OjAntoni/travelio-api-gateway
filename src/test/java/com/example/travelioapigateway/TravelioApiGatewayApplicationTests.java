73f09d3ebb16926e95496dcd624873170fd18c29
ojAntoni <zezyulin90@gmail.com>
2023-10-24 22:39:17 +0200

    Initial commit

---
+++ src/test/java/com/example/travelioapigateway/TravelioApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.travelioapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelioApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
